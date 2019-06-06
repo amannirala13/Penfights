eb77663d1d13a240dc3abc45191edb862a0a0814
amannirala13 <amansofttechinfo@gmail.com>
2019-06-06 22:54:24 +0530

    Added basic activities and set the primary colors. Themes edited

---
+++ app/src/main/java/com/asdev/penfights/FirstRun.java
@@ -0,0 +1,13 @@
+package com.asdev.penfights;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstRun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_run);
+    }
+}
